c60d2f22f24b019a05a3e4db34adfeff0b362c6d
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-01-06 15:22:05 +0530

    arrow function and object creation

--- first-app.js
+++ first-app.js
@@ -3,3 +3,17 @@
 // fs.writeFileSync('hello.txt','hello from sagar')
 
 console.log("hello world")
+
+const multi = (a,b) => a*b ;
+
+console.log(multi(5,6));
+
+const student = {
+
+    name: 'sagar sethi',
+    age: 24,
+    roll_no: 39
+
+}
+
+console.log(student);
