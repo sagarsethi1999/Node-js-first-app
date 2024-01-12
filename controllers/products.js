6c26695fa793209dbba201a48b3986263e49f4d7
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-01-12 13:45:13 +0530

    models added

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,8 @@
+const Product = require('../models/product')
+const fs = require('fs')
 const path = require('path')
+
+
 exports.getAddproduct=(req, res, next) => {
     res.sendFile(path.join(__dirname,'..','views','add-product.html'));
     
@@ -6,9 +10,29 @@ exports.getAddproduct=(req, res, next) => {
 
   exports.postAddproduct=(req, res, next) => {
     console.log(req.body);
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
   }
 
   exports.getProducts =  (req, res, next) => {
-    res.sendFile(path.join(__dirname,'..', 'views','shop.html'));
+     Product.fetchall((products => {
+        // res.sendFile(path.join(__dirname,'..', 'views','shop.html'));
+        // Read the HTML template file
+        const htmlPath = path.join(__dirname, '..', 'views', 'shop.html');
+        fs.readFile(htmlPath, 'utf8', (err, htmlTemplate) => {
+            if (err) {
+                console.error('Error reading HTML template:', err);
+                return res.status(500).send('Internal Server Error');
+            }
+
+            // Replace a placeholder in the HTML template with product data
+            const productListHTML = products.map(product => `<li>${product.title}</li>`).join('');
+            const updatedHTML = htmlTemplate.replace('{{productList}}', productListHTML);
+
+            // Send the updated HTML to the client
+            res.send(updatedHTML);
+        });
+    }));
+    
   }
